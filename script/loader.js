294c71b2ebc62e2350b5eea1242147da2c969d7b
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-26 16:23:06 +0100

    Refactor: Clear the repo for the init

--- script/loader.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loader = document.getElementById("loader");
-    loader.style.opacity = 0;
-    setTimeout(() => loader.style.display = "none", 500);
-});
